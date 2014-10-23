2615e14329bc20a082ab8518346bd0245673b34e
Juliusz Gonera <juliusz.gonera@gmail.com>
2014-10-23 05:39:05 +0200

    Add code for loading reuters data

--- src/Scratch.scala
+++ src/Scratch.scala
@@ -1,3 +1,6 @@
+package msc
+package classifier
+
 import weka.core._
 import weka.core.converters._
 import weka.classifiers.trees._
@@ -7,8 +10,14 @@ import java.io._
 import weka.classifiers.functions.LibLINEAR
 import weka.classifiers.Evaluation
 import java.util.Random
+import weka.core.converters.ConverterUtils.DataSource
 
 object Scratch {
+  type TrainingSet = Instances
+  type TestSet = Instances
+  type DatasetCallback[A] = (TrainingSet, TestSet) => A
+  val reutersDatasetPath = "datasets/reuters21578-modApte/"
+
   def loadData(): Instances = {
     val loader: TextDirectoryLoader = new TextDirectoryLoader()
     loader.setDirectory(new File("datasets/guardian.co.uk/"))
@@ -16,6 +25,31 @@ object Scratch {
     val filter: StringToWordVector = new StringToWordVector()
     filter.setInputFormat(dataRaw)
     Filter.useFilter(dataRaw, filter)
+  }
+
+  // Reads in reuters21578-modApte dataset
+  def withReutersData[A](trainFileName: String, testFileName: String)
+                        (f: DatasetCallback[A]): Unit = {
+    
+    val train = loadReutersFile(trainFileName)
+    val test = loadReutersFile(testFileName)
+    f(train, test)
+    // val fileList = new File(datasetPath).listFiles().filter { (file: File) =>
+    //   file.getName.matches(".*.arff")
+    // }
+
+    // for (file <- fileList) {
+    //    new DataSource(fileName).getDataSet()
+    //   source.getDataSet()
+    // }
+  }
+
+  def loadReutersFile(fileName: String): Instances = {
+    val instances =
+      new DataSource(reutersDatasetPath + fileName).getDataSet()
+    instances.setClassIndex(0)
+
+    instances
   }
 
   def randomizeData(data: Instances): Instances = {
@@ -26,7 +60,7 @@ object Scratch {
   }
 
   def runCV[A](data: Instances, folds: Integer)
-           (f: (Instances, Instances) => A): List[A] = {
+           (f: DatasetCallback[A]): List[A] = {
     val randData = randomizeData(data)
 
     val lst = for ( n <- 0 until folds ) yield {
@@ -40,16 +74,53 @@ object Scratch {
   }
 
   def run() = {
-    val data = loadData()
+    // val data = loadData()
 
-    val lst = runCV(data, 2){ (train, test) => {
-                               val evaluation = new Evaluation(data)
-                               val classifier: LibLINEAR = new LibLINEAR()
-                               classifier.buildClassifier(train)
-                               evaluation.evaluateModel(classifier, test)
-                               println(evaluation.toSummaryString())
-                             }
+    // val lst = runCV(data, 2){ (train, test) => {
+    //                            val evaluation = new Evaluation(data)
+    //                            val classifier: LibLINEAR = new LibLINEAR()
+    //                            classifier.buildClassifier(train)
+    //                            evaluation.evaluateModel(classifier, test)
+    //                            println(evaluation.toSummaryString())
+    //                          }
+
+    // Instances trainInstances = ... instances got from somewhere
+    // Instances testInstances = ... instances got from somewhere
+    // Classifier scheme = ... scheme got from somewhere
+    
+    // Evaluation evaluation = new Evaluation(trainInstances);
+    // evaluation.evaluateModel(scheme, testInstances);
+    // System.out.println(evaluation.toSummaryString());
+    val data = withReutersData("reutersAcqModApteTest-FullVocab.arff",
+                               "reutersAcqModApteTrain-FullVocab.arff") {
+      (train, test) => {
+        val classifier: LibLINEAR = new LibLINEAR()
+        classifier.buildClassifier(train)
+        val evaluation = new Evaluation(train)
+        evaluation.evaluateModel(classifier, test)
+        val precision: Double = evaluation.precision(0)
+        val recall: Double = evaluation.recall(0)
+        val fMeasure: Double = evaluation.fMeasure(0)
+        val confusionMatrix: Array[Array[Double]] = evaluation.confusionMatrix()
+        println(f"Precision: $precision%2.3f")
+        println(f"Recall: $recall%2.3f")
+        println(f"F-Measure: $fMeasure%2.3f")
+
+        println()
+        println("Confusion Matrix: ")
+        println(f"${confusionMatrix(0)(0)}%4.0f ${confusionMatrix(0)(1)}%4.0f")
+        println(f"${confusionMatrix(1)(0)}%4.0f ${confusionMatrix(1)(1)}%4.0f")
+      }
     }
+
+    // val lst = runCV(data, 2){ (train, test) => {
+    //                            val evaluation = new Evaluation(data)
+    //                            val classifier: LibLINEAR = new LibLINEAR()
+    //                            classifier.buildClassifier(train)
+    //                            evaluation.evaluateModel(classifier, test)
+    //                            println(evaluation.toSummaryString())
+    //                          }
+    // }
   }
   // System.out.println(evaluation.toSummaryString());
 
